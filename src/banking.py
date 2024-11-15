72e71edf478e02c6f7222a1639d681674be6e8d5
dydwns123123 <yongjunan111@gmail.com>
2024-11-15 17:09:06 +0900

    feat: implements NormalMember
    normal member에 대한 이자율 추가
    
    ticket: resolve KAN-13

--- src/banking.py
+++ src/banking.py
@@ -12,3 +12,11 @@ class Member(InterestStrategy):
         print('No Discount')
 
         return money
+    
+    
+class NormalMember(InterestStrategy):
+    def apply_interest(self, money):
+        interest = money * 0.0022
+        print(f'NormalMember interest: {interest}')
+
+        return money + interest
